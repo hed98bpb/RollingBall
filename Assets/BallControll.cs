f7b4be540267d7297ac4b82b866046d580c3a5fd
Peter Burgaard <Peter Burgaard>
2016-05-01 00:55:13 +0200

    first push of the rolling ball project

---
+++ Assets/BallControll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControll : MonoBehaviour {
+
+    float rotationSpeed = 100;
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        rotation *= Time.deltaTime;
+        rb.AddRelativeTorque(Vector3.back * rotation);
+    }
+    
+}
