af07c13d10d67d8c04e026f655bb3c32ee0c2d9a
hed98bpb <loleparty@yahoo.dk>
2016-05-01 02:06:01 +0200

    the ball can now jump!

--- Assets/BallControll.cs
+++ Assets/BallControll.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class BallControll : MonoBehaviour {
 
-    float rotationSpeed = 100;
+    private bool isFalling = false;
+
+    int rotationSpeed = 100;
+    int jumpHeight = 8;
     public Rigidbody rb;
 
     // Use this for initialization
@@ -13,9 +16,21 @@ public class BallControll : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
         rotation *= Time.deltaTime;
         rb.AddRelativeTorque(Vector3.back * rotation);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isFalling == false) {
+            rb.velocity = new Vector3(rb.velocity.x, jumpHeight, rb.velocity.z);
+        }
+
+        isFalling = true;
+    }
+
+    void OnCollisionStay()
+    {
+        isFalling = false;
     }
     
 }
